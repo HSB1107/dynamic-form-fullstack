e9c5476575f40e657bed9783ff614f1342cbdaa8
Hrishikesh Bakshi <bakshihrishikesh1107@gmail.com>
2026-02-28 19:17:10 +0530

    init(server): initialize the sprinboot project with the initial required dependencies and application.properties file

---
+++ server/src/test/java/com/hsb/dynamicform/DynamicformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsb.dynamicform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
